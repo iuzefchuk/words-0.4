cd32e0cf2165b2a3da0b728c24923d45791866d4
kat <yuzefchukk@gmail.com>
2026-04-27 11:56:05 +0300

    port env fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,51 @@
 import path from 'node:path';
 import vue from '@vitejs/plugin-vue';
+import { loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1000,
-    emptyOutDir: true,
-    outDir: path.resolve(__dirname, 'dist'),
-    target: 'esnext',
-  },
-  define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
-  },
-  optimizeDeps: {
-    include: ['vue', 'pinia'],
-  },
-  plugins: [
-    vue(),
-    {
-      configureServer(server) {
-        server.middlewares.use((_, res, next) => {
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-          next();
-        });
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname, '');
+  return {
+    build: {
+      chunkSizeWarningLimit: 1000,
+      emptyOutDir: true,
+      outDir: path.resolve(__dirname, 'dist'),
+      target: 'esnext',
+    },
+    define: {
+      APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    },
+    optimizeDeps: {
+      include: ['vue', 'pinia'],
+    },
+    plugins: [
+      vue(),
+      {
+        configureServer(server) {
+          server.middlewares.use((_, res, next) => {
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+            next();
+          });
+        },
+        name: 'cross-origin-isolation',
       },
-      name: 'cross-origin-isolation',
+    ],
+    publicDir: path.resolve(__dirname, 'public'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  ],
-  publicDir: path.resolve(__dirname, 'public'),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    root: path.resolve(__dirname, './src/interface'),
+    server: {
+      port: Number.isNaN(Number(env.VITE_PORT)) || Number(env.VITE_PORT) === 0 ? 5173 : Number(env.VITE_PORT),
     },
-  },
-  root: path.resolve(__dirname, './src/interface'),
-  server: {
-    port:
-      Number(process.env.VITE_PORT) !== 0 && !Number.isNaN(Number(process.env.VITE_PORT)) ? Number(process.env.VITE_PORT) : 5173,
-  },
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-    include: ['tests/**/*.test.ts'],
-    restoreMocks: true,
-  },
+    test: {
+      environment: 'happy-dom',
+      globals: true,
+      include: ['tests/**/*.test.ts'],
+      restoreMocks: true,
+    },
+  };
 });
